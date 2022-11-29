6fc21591e09dd8464f014fcdfe99db57188a64de
acnixuil <35956405+acnixuil@users.noreply.github.com>
2022-11-30 06:56:26 +0800

    Update n1.sh

--- n1.sh
+++ n1.sh
@@ -1,4 +1,5 @@
 # firewall custom
-echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+#echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+echo "iptables -t nat -I POSTROUTING -j FULLCONENAT" >> package/network/config/firewall/files/firewall.user
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
