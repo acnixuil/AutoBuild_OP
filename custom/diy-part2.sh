c2ab883f9c53e11ad93c209336e95f29d7db206e
acnixuil <acnixuil@gmail.com>
2023-01-22 20:47:56 +0800

    update

--- custom/diy-part2.sh
+++ custom/diy-part2.sh
@@ -10,4 +10,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.88/g' package/base-files/files/bin/config_generate
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/base-files/files/etc/shadow
