f0d222ee93e0392ca6321f15d1cad2eb4d1cb633
acnixuil <acnixuil@gmail.com>
2023-02-08 23:01:27 +0800

    update

--- custom/x86-64.sh
+++ custom/x86-64.sh
@@ -8,6 +8,9 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# 修改内核版本
+sed -i '/.*KERNEL_PATCHVER*/c\KERNEL_PATCHVER:=5.15' /home/lede/target/linux/x86/Makefile
+# 修改网关ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 取消登录密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
