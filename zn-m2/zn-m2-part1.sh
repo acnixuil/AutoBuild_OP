6492c6d0f283ee012e44bc9000f1782052b4eb8d
acnixuil <acnixuil@gmail.com>
2023-03-13 01:16:24 +0800

    update

--- zn-m2/zn-m2-part1.sh
+++ zn-m2/zn-m2-part1.sh
@@ -2,4 +2,4 @@
 
 # passwall
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
-#sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-new-version' feeds.conf.default
