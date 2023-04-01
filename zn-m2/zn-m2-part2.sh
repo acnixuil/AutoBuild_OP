9a7e6f8cdd92c73931acc743000f1fc9a6e2542d
acnixuil <acnixuil@gmail.com>
2023-04-01 17:33:11 +0800

    æ·切换passwallå新luciâ

--- zn-m2/zn-m2-part2.sh
+++ zn-m2/zn-m2-part2.sh
@@ -24,4 +24,4 @@ git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-d
 # passwall
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-new-version ./package/passwall
