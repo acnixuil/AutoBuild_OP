e073415008249930c6babdd5f6b6642c0a7284b3
acnixuil <acnixuil@gmail.com>
2023-04-06 10:32:25 +0800

    feat: t7新配置

--- 360_t7/diy-part2.sh
+++ 360_t7/diy-part2.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 #============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# 以下部分为更新并安装feeds后后后运行
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 添加adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+
+# 更换design为最新版本
+rm -rf feeds/luci/themes/luci-theme-design
+git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+
+# 更换argon为最新版本
+rm -rf feeds/luci/themes/luci-theme-argon*
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+# passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
+#git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-new-version ./package/passwall
