d7030c994585a3a44d0c196753a231f505a268d7
acnixuil <acnixuil@gmail.com>
2023-04-14 05:12:34 +0800

    fix: mosdns

--- 360_t7/diy-part2.sh
+++ 360_t7/diy-part2.sh
@@ -30,5 +30,4 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/pa
 rm -rf feeds/packages/net/mosdns*
 rm -rf feeds/luci/applications/luci-app-mosdns*
 rm -rf feeds/packages/net/v2ray-geodata*
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
