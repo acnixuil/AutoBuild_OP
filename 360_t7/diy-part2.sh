9ba6a80021be56c58e712f365329d2a2d79306cb
acnixuil <35956405+acnixuil@users.noreply.github.com>
2023-04-21 13:13:36 +0800

    Update diy-part2.sh

--- 360_t7/diy-part2.sh
+++ 360_t7/diy-part2.sh
@@ -32,7 +32,7 @@ rm -rf feeds/packages/net/mosdns*
 rm -rf feeds/luci/applications/luci-app-mosdns*
 rm -rf feeds/packages/net/v2ray-geodata*
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 #./scripts/feeds update -a
 #./scripts/feeds install -a
