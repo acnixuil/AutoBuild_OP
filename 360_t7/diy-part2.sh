3d13eb82ceb55d8f24f02ac9929b5fc06b2bfa0b
acnixuil <35956405+acnixuil@users.noreply.github.com>
2023-04-21 13:15:50 +0800

    Update diy-part2.sh

--- 360_t7/diy-part2.sh
+++ 360_t7/diy-part2.sh
@@ -28,8 +28,8 @@ rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall
 
 # mosdns
-rm -rf feeds/packages/net/mosdns*
-rm -rf feeds/luci/applications/luci-app-mosdns*
+#rm -rf feeds/packages/net/mosdns*
+#rm -rf feeds/luci/applications/luci-app-mosdns*
 #rm -rf feeds/packages/net/v2ray-geodata*
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
