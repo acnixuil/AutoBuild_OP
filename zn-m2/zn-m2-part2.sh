1f83d4df1b500d7560aed2f55e717fe360e6a74f
acnixuil <acnixuil@gmail.com>
2023-05-04 10:56:43 +0800

    fix:

--- zn-m2/zn-m2-part2.sh
+++ zn-m2/zn-m2-part2.sh
@@ -9,7 +9,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 
 # 更换design为最新版本
 rm -rf feeds/luci/themes/luci-theme-design*
@@ -23,9 +22,9 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # passwall
-rm -rf feeds/luci/applications/luci-app-passwall*
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-new-version ./package/passwall
+#rm -rf feeds/luci/applications/luci-app-passwall*
+#git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
+#git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall
 
 # mosdns
 rm -rf feeds/packages/net/mosdns*
@@ -34,3 +33,5 @@ rm -rf feeds/packages/net/v2ray-geodata*
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+# 向导
+#git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
