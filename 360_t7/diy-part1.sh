242e4370d0b5ffe5a209ec24dba7d4ec59960c63
acnixuil <acnixuil@gmail.com>
2023-06-30 17:20:08 +0800

    update:

--- 360_t7/diy-part1.sh
+++ 360_t7/diy-part1.sh
@@ -1,2 +1,3 @@
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
 #sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
