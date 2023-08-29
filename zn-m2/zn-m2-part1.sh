66ee79dc345b91053262289aee001c2f3654d4b1
acnixuil <acnixuil@gmail.com>
2023-08-30 07:47:15 +0800

    fix

--- zn-m2/zn-m2-part1.sh
+++ zn-m2/zn-m2-part1.sh
@@ -1,5 +1,4 @@
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
 # passwall
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
 #sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
