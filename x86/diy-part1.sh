c2e47b2c031a309c0c41f1f25b162eb9f19c074c
acnixuil <acnixuil@gmail.com>
2023-09-03 04:48:32 +0800

    fix

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -1,13 +1,11 @@
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-# 添加passwall
+# passwall
 sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
 
-
 mkdir wget
-
 
 cat>rename.sh<<-\EOF
 #!/bin/bash
