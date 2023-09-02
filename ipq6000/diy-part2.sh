104413d51c332b49bb87432864bc78f041df7628
acnixuil <acnixuil@gmail.com>
2023-09-03 03:55:12 +0800

    fix

--- ipq6000/diy-part2.sh
+++ ipq6000/diy-part2.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-#============================================================
-# 以下部分为更新并安装feeds后后后运行
-#============================================================
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 
-echo "开始 DIY2 配置……"
-echo "========================="
-
-
-# 修改内核版本
-#sed -i '/.*KERNEL_PATCHVER*/c\KERNEL_PATCHVER:=5.15' target/linux/x86/Makefile
 # 修改网关ip
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-# 取消登录密码
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-# 修改默认主题
-#sed -i 's/luci-theme-argon/luci-theme-bootstrap/' feeds/luci/collections/luci/Makefile
-
-# 配置alist3编译环境
-#rm -rf feeds/packages/lang/golang
-#svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
-
-# 添加alist
-#git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # 添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
