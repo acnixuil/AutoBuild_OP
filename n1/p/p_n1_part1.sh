51d84e0054ba006bbdcaa3f42f1f2dc085fe005f
acnixuil <acnixuil@gmail.com>
2023-09-18 18:21:01 +0800

    fix

--- n1/p/p_n1_part1.sh
+++ n1/p/p_n1_part1.sh
@@ -2,5 +2,5 @@
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add passwall
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
