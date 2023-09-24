83c5ebc2444c8bf3ce5be41adacb405f93fdb445
acnixuil <acnixuil@gmail.com>
2023-09-25 04:12:52 +0800

    fix

--- x86_imm/diy-part2.sh
+++ x86_imm/diy-part2.sh
@@ -6,20 +6,12 @@
 echo "开始 DIY2 配置……"
 echo "========================="
 
-
-# 修改内核版本
-#sed -i '/.*KERNEL_PATCHVER*/c\KERNEL_PATCHVER:=5.15' target/linux/x86/Makefile
 # 修改网关ip
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-# 取消登录密码
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-# 修改默认主题
-#sed -i 's/luci-theme-argon/luci-theme-bootstrap/' feeds/luci/collections/luci/Makefile
 
 # 添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-#git clone https://github.com/kenzok78/luci-app-adguardhome.git luci-app-adguardhome
 
 # 更换design为最新版本
 #rm -rf feeds/luci/themes/luci-theme-design
@@ -32,8 +24,6 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # mosdns
-#rm -rf feeds/packages/net/mosdns*
-#rm -rf feeds/luci/applications/luci-app-mosdns*
 rm -rf feeds/packages/net/v2ray-geodata*
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
