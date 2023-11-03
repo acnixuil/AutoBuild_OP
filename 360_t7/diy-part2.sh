029f6f3b57bbfe9aa9946970bf4cb2da60dcf608
acnixuil <acnixuil@gmail.com>
2023-11-03 10:52:14 +0800

    fix

--- 360_t7/diy-part2.sh
+++ 360_t7/diy-part2.sh
@@ -23,14 +23,12 @@ git clone https://github.com/acnixuil/luci-app-adguardhome.git package/luci-app-
 #git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
 
 # mosdns
-#rm -f package/feeds/packages/mosdns
-#rm -rf feeds/packages/net/mosdns
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-#cp -f package/v2ray-geodata/Makefile feeds/packages/net/v2ray-geodata/Makefile
+rm -f package/feeds/packages/mosdns
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata*
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+cp -f package/v2ray-geodata/Makefile feeds/packages/net/v2ray-geodata/Makefile
 
 # Enable Cache
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
