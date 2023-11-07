1ac43d963b9db7fc222e7f7c9b24a31f6e2e5c78
acnixuil <acnixuil@gmail.com>
2023-11-08 05:09:00 +0800

    fix

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -5,16 +5,15 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
-mkdir wget
-
-cat>del.sh<<-\EOF
+# 裁剪多余文件并重命名输出文件
+cat <<EOL > del.sh
 #!/bin/bash
 rm -rf packages
 rm -rf bin/targets/x86/64/config.buildinfo
 rm -rf bin/targets/x86/64/feeds.buildinfo
 rm -rf bin/targets/x86/64/openwrt-x86-64-generic.manifest
 rm -rf bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin
-#rm -rf bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
+rm -rf bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
 rm -rf bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.vmdk
 rm -rf bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz
 rm -rf bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
@@ -24,26 +23,26 @@ rm -rf bin/targets/x86/64/version.buildinfo
 sleep 2
 
 # 提取默认内核版本号
-str1=$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2)
+str1=\$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2)
 
 # 构建目标文件的路径
-kernel_file="include/kernel-${str1}"
+kernel_file="include/kernel-\$str1"
 
 # 提取内核版本号
-Kernel=$(grep -o "LINUX_VERSION-${str1} = .*" "$kernel_file" | cut -d ' ' -f 3)
-echo "KERNEL=$Kernel" >> $GITHUB_ENV
+Kernel=\$(grep -o "LINUX_VERSION-\$str1 = .*" "\$kernel_file" | cut -d ' ' -f 3)
+echo "内核版本为${Kernel}"
+echo "KERNEL=\$Kernel" >> \$GITHUB_ENV
 
 # 检查文件是否存在，然后重命名
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz "bin/targets/x86/64/openwrt_x86-64_${Kernel}_uefi.img.gz"
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz "bin/targets/x86/64/openwrt_x86-64_\$Kernel_uefi.img.gz"
 fi
 
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz "bin/targets/x86/64/openwrt_x86-64_${Kernel}_bios.img.gz"
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz "bin/targets/x86/64/openwrt_x86-64_\$Kernel_bios.img.gz"
 fi
 
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk "bin/targets/x86/64/openwrt_x86-64_${Kernel}_uefi.vmdk"
-
-exit 0
-EOF
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk "bin/targets/x86/64/openwrt_x86-64_\$Kernel_uefi.vmdk"
+fi
+EOL
--- x86_imm/diy-part1.sh
+++ x86_imm/diy-part1.sh
@@ -5,7 +5,8 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
-cat>del.sh<<-\EOF
+# 裁剪多余文件并重命名输出文件
+cat <<EOL > del.sh
 #!/bin/bash
 rm -rf packages
 rm -rf bin/targets/x86/64/config.buildinfo
@@ -29,6 +30,7 @@ kernel_file="include/kernel-${str1}"
 
 # 提取内核版本号
 Kernel=$(grep -o "LINUX_VERSION-${str1} = .*" "$kernel_file" | cut -d ' ' -f 3)
+echo "内核版本为${Kernel}"
 echo "KERNEL=$Kernel" >> $GITHUB_ENV
 
 # 检查文件是否存在，然后重命名
@@ -43,6 +45,4 @@ fi
 if [ -f bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk ]; then
     mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk "bin/targets/x86/64/immortalwrt_x86-64_${Kernel}_uefi.vmdk"
 fi
-
-exit 0
 EOF
