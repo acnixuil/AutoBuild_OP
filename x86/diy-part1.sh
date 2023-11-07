84bf4fccbb7e28df722f1a9e4777e6eb8d7f83ab
acnixuil <acnixuil@gmail.com>
2023-11-08 06:27:28 +0800

    fix

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -21,28 +21,26 @@ rm -rf bin/targets/x86/64/profiles.json
 rm -rf bin/targets/x86/64/sha256sums
 rm -rf bin/targets/x86/64/version.buildinfo
 sleep 2
-
-# 提取默认内核版本号
-str1=\$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2)
-
+# 获取str1的值
+str1=$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2)
 # 构建目标文件的路径
-kernel_file="include/kernel-\$str1"
-
+kernel_file="include/kernel-${str1}"
 # 提取内核版本号
-Kernel=\$(grep -o "LINUX_VERSION-\$str1 = .*" "\$kernel_file" | cut -d ' ' -f 3)
-echo "内核版本为${Kernel}"
-echo "KERNEL=$Kernel" >> \$GITHUB_ENV
-
+kernel_version=$(grep -o "LINUX_VERSION-${str1} = .*" "$kernel_file" | cut -d ' ' -f 3)
+# 拼接str1和kernel_version以获得完整的kernel
+kernel="${str1}${kernel_version}"
+# 打印kernel变量的值
+echo "Kernel version: $kernel"
 # 检查文件是否存在，然后重命名
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz "bin/targets/x86/64/openwrt_x86-64_${Kernel}_uefi.img.gz"
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz "bin/targets/x86/64/openwrt_x86-64_${kernel}_uefi.img.gz"
 fi
 
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz "bin/targets/x86/64/openwrt_x86-64_${Kernel}_bios.img.gz"
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz "bin/targets/x86/64/openwrt_x86-64_${kernel}_bios.img.gz"
 fi
 
 if [ -f bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk ]; then
-    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk "bin/targets/x86/64/openwrt_x86-64_${Kernel}_uefi.vmdk"
+    mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk "bin/targets/x86/64/openwrt_x86-64_${kernel}_uefi.vmdk"
 fi
 EOL
