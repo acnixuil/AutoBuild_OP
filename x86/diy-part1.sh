675f302384d294eae50cdd388d3df4de5249db14
acnixuil <acnixuil@gmail.com>
2023-11-09 07:28:05 +0800

    fix

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -22,29 +22,30 @@ rm -rf bin/targets/x86/64/profiles.json
 rm -rf bin/targets/x86/64/sha256sums
 rm -rf bin/targets/x86/64/version.buildinfo
 sleep 2
-str1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
-ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-5.4 | cut -d . -f 3`
-ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-5.10 | cut -d . -f 3`
-ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
-ver61=`grep "LINUX_VERSION-6.1 ="  include/kernel-6.1 | cut -d . -f 3`
-if [ "$str1" = "5.4" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.vmdk
-elif [ "$str1" = "5.15" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver515}_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver515}_uefi.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/openwrt_x86-64_${str1}.${ver515}_uefi.vmdk
-elif [ "$str1" = "6.1" ];then
+str1=`grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2` # Determine the current default kernel version like 5.10
+ver54=`grep "LINUX_VERSION-5.4 =" include/kernel-5.4 | cut -d . -f 3`
+ver510=`grep "LINUX_VERSION-5.10 =" include/kernel-5.10 | cut -d . -f 3`
+ver515=`grep "LINUX_VERSION-5.15 =" include/kernel-5.15 | cut -d . -f 3`
+ver61=`grep "LINUX_VERSION-6.1 =" include/kernel-6.1 | cut -d . -f 3`
+if [ "$str1" = "5.4" ]; then
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_bios.img.gz
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.img.gz
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.vmdk
+elif [ "$str1" = "5.15" ]; then
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/openwrt_x86-64_${str1}.${ver515}_bios.img.gz
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/openwrt_x86-64_${str1.${ver515}_uefi.img.gz
+   mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/openwrt_x86-64_${str1.${ver515}_uefi.vmdk
+elif [ "$str1" = "6.1" ]; then
    if [ ! $ver61 ]; then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_uefi.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/openwrt_x86-64_uefi.vmdk
-  else
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver61}_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver61}_uefi.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/openwrt_x86-64_${str1}.${ver61}_uefi.vmdk
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/openwrt_x86-64_bios.img.gz
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/openwrt_x86-64_uefi.img.gz
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/openwrt_x86-64_uefi.vmdk
+   else
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/openwrt_x86-64_${str1.${ver61}_bios.img.gz
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/openwrt_x86-64_${str1.${ver61}_uefi.img.gz
+      mv bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/openwrt_x86-64_${str1.${ver61}_uefi.vmdk
    fi
 fi
-ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
+ls -l "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
+exit 0
 EOL
