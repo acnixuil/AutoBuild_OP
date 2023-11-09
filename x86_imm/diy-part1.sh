cd0dee2acd1e2537a9bee475b416499d8e37db0f
acnixuil <acnixuil@gmail.com>
2023-11-09 09:00:14 +0800

    fix

--- x86_imm/diy-part1.sh
+++ x86_imm/diy-part1.sh
@@ -8,7 +8,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 
 mkdir wget
 # 裁剪多余文件并重命名输出文件
-cat <<EOL > rename.sh
+cat > rename.sh <<-EOF
 #!/bin/bash
 rm -rf bin/targets/x86/64/packages
 rm -rf bin/targets/x86/64/config.buildinfo
@@ -25,30 +25,37 @@ rm -rf bin/targets/x86/64/profiles.json
 rm -rf bin/targets/x86/64/sha256sums
 rm -rf bin/targets/x86/64/version.buildinfo
 sleep 2
-str1=`grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2` # Determine the current default kernel version like 5.10
-ver54=`grep "LINUX_VERSION-5.4 =" include/kernel-5.4 | cut -d . -f 3`
-ver510=`grep "LINUX_VERSION-5.10 =" include/kernel-5.10 | cut -d . -f 3`
-ver515=`grep "LINUX_VERSION-5.15 =" include/kernel-5.15 | cut -d . -f 3`
-ver61=`grep "LINUX_VERSION-6.1 =" include/kernel-6.1 | cut -d . -f 3`
-if [ "$str1" = "5.4" ]; then
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_bios.img.gz
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_uefi.img.gz
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_uefi.vmdk
-elif [ "$str1" = "5.15" ]; then
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_bios.img.gz
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_uefi.img.gz
-    mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_uefi.vmdk
-elif [ "$str1" = "6.1" ]; then
-    if [ ! $ver61 ]; then
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_bios.img.gz
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_uefi.img.gz
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_uefi.vmdk
-    else
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_bios.img.gz
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_uefi.img.gz
-        mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_uefi.vmdk
-    fi
+str1=\$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2) # Determine the current default kernel version like 5.10
+ver54=\$(grep "LINUX_VERSION-5.4 =" include/kernel-5.4 | cut -d . -f 3)
+ver510=\$(grep "LINUX_VERSION-5.10 =" include/kernel-5.10 | cut -d . -f 3)
+ver515=\$(grep "LINUX_VERSION-5.15 =" include/kernel-5.15 | cut -d . -f 3)
+ver61=\$(grep "LINUX_VERSION-6.1 =" include/kernel-6.1 | cut -d . -f 3)
+if [ "\$str1" = "5.4" ]; then
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver54}_bios.img.gz
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver54}_uefi.img.gz
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver54}_uefi.vmdk
+elif [ "\$str1" = "5.15" ]; then
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver515}_bios.img.gz
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver515}_uefi.img.gz
+   mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver515}_uefi.vmdk
+elif [ "\$str1" = "6.1" ]; then
+   if [ ! \$ver61 ]; then
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_bios.img.gz
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_uefi.img.gz
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_uefi.vmdk
+   else
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver61}_bios.img.gz
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver61}_uefi.img.gz
+      mv bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk bin/targets/x86/64/immortalwrt_x86-64_\${str1}.\${ver61}_uefi.vmdk
+   fi
 fi
-ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
+# md5
+ls -l "bin/targets/x86/64" | awk -F " " '{print \$9}' > wget/open_dev_md5
+dev_version=\$(grep "_uefi.img.gz" wget/open_dev_md5 | cut -d - -f 2 | cut -d _ -f 2)
+immortalwrt_dev=immortalwrt_x86-64_\${dev_version}_bios.img.gz
+immortalwrt_dev_uefi=immortalwrt_x86-64_\${dev_version}_uefi.img.gz
+cd bin/targets/x86/64
+# md5sum \$immortalwrt_dev > immortalwrt_bios.md5
+# md5sum \$immortalwrt_dev_uefi > immortalwrt_uefi.md5
 exit 0
-EOL
+EOF
