b1368d6827038c10cb1565fd03159bc88817b1e2
acnixuil <acnixuil@gmail.com>
2023-11-13 04:46:26 +0800

    fix

--- x86_imm/diy-part1.sh
+++ x86_imm/diy-part1.sh
@@ -8,7 +8,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 
 mkdir wget
 # 裁剪多余文件并重命名输出文件
-cat > rename.sh <<-EOF
+cat> rename.sh << 'EOF'
 #!/bin/bash
 rm -rf bin/targets/x86/64/packages
 rm -rf bin/targets/x86/64/config.buildinfo
@@ -28,37 +28,46 @@ rm -rf bin/targets/x86/64/profiles.json
 rm -rf bin/targets/x86/64/sha256sums
 rm -rf bin/targets/x86/64/version.buildinfo
 sleep 2
-str1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
-ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-5.4 | cut -d . -f 3`
-ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-5.10 | cut -d . -f 3`
-ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
-ver61=`grep "LINUX_VERSION-6.1 ="  include/kernel-6.1 | cut -d . -f 3`
-if [ "$str1" = "5.4" ];then
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_bios.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_uefi.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk      bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver54}_uefi.vmdk
-elif [ "$str1" = "5.15" ];then
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_bios.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_uefi.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver515}_uefi.vmdk
-elif [ "$str1" = "6.1" ];then
-   if [ ! $ver61 ]; then
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64_bios.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64_uefi.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/immortalwrt_x86-64_uefi.vmdk
-  else
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_bios.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_uefi.img.gz
-   mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk     bin/targets/x86/64/immortalwrt_x86-64_${str1}.${ver61}_uefi.vmdk
-   fi
+
+# 获取当前默认内核版本号如5.10
+str1=$(grep "KERNEL_PATCHVER:=" target/linux/x86/Makefile | cut -d = -f 2)
+echo "当前默认内核版本号：${str1}"
+
+# 检查是否存在对应版本的内核文件
+kernel_file="include/kernel-${str1}"
+if [ -f "$kernel_file" ]; then
+    echo "文件 $kernel_file 存在."
+    # 寻找版本号具体数字写入ver变量
+    ver=$(grep "LINUX_VERSION-${str1} =" "$kernel_file" | cut -d . -f 3)
+    echo "版本号：${ver}"
+
+    # 构建目标文件路径
+    target_path="bin/targets/x86/64/"
+
+    # 判断是否是 6.1 版本
+    if [ "$str1" = "6.1" ]; then
+        if [ -z "$ver" ]; then
+            # 版本号为空，表示 6.1 版本不存在，重命名到 _bios/_uefi
+            echo "版本号为空，表示 6.1 版本不存在，重命名到 _bios/_uefi"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined.img.gz" "${target_path}immortalwrt_x86-64_bios.img.gz"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz" "${target_path}immortalwrt_x86-64_uefi.img.gz"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk" "${target_path}immortalwrt_x86-64_uefi.vmdk"
+        else
+            # 版本号不为空，表示 6.1 版本存在，重命名到对应版本
+            echo "版本号不为空，表示 6.1 版本存在，重命名到对应版本"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined.img.gz" "${target_path}immortalwrt_x86-64_${str1}.${ver}_bios.img.gz"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz" "${target_path}immortalwrt_x86-64_${str1}.${ver}_uefi.img.gz"
+            mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk" "${target_path}immortalwrt_x86-64_${str1}.${ver}_uefi.vmdk"
+        fi
+    else
+        # 不是 6.1 版本，重命名到对应版本
+        echo "不是 6.1 版本，重命名到对应版本"
+        mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined.img.gz" "${target_path}immortalwrt_x86-64_${str1}.${ver}_bios.img.gz"
+        mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz" "${target_path}immortalwrt_x86-64_${str1}.${ver}_uefi.img.gz"
+        mv "${target_path}immortalwrt-x86-64-generic-squashfs-combined-efi.vmdk" "${target_path}immortalwrt_x86-64_${str1}.${ver}_uefi.vmdk"
+    fi
+else
+    echo "文件 $kernel_file 不存在."
 fi
-#md5
-ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
-dev_version=`grep "_uefi.img.gz" wget/open_dev_md5 | cut -d - -f 2 | cut -d _ -f 2 `
-immortalwrt_dev=immortalwrt_x86-64_${dev_version}_bios.img.gz
-immortalwrt_dev_uefi=immortalwrt_x86-64_${dev_version}_uefi.img.gz
-cd bin/targets/x86/64
-#md5sum $immortalwrt_dev > immortalwrt_bios.md5
-#md5sum $immortalwrt_dev_uefi > immortalwrt_uefi.md5
 exit 0
 EOF
