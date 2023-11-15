73b66480ee97a13f04a2568a63ee9ef59bcbb7bf
acnixuil <acnixuil@gmail.com>
2023-11-16 04:40:16 +0800

    fix

--- ipq6000/diy-part1.sh
+++ ipq6000/diy-part1.sh
@@ -8,5 +8,5 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-./scripts/feeds install -a -f -p passwall_packages
-./scripts/feeds install -a -f -p passwall_luci
+#./scripts/feeds install -a -f -p passwall_packages
+#./scripts/feeds install -a -f -p passwall_luci
