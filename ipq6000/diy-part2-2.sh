2c0b0525960b40d3d2b734d7793c5d9519ea7ec2
acnixuil <acnixuil@gmail.com>
2023-11-15 11:54:23 +0800

    fix

---
+++ ipq6000/diy-part2-2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
+# Enable Cache
+#echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
+
+echo "========================="
+echo " DIY2 配置完成……"
