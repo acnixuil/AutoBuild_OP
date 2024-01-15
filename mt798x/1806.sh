bb8e44d3a4cd8facbe35bbc4aa584715994026d1
acnixuil <acnixuil@gmail.com>
2024-01-15 21:27:40 +0800

    fix

--- mt798x/1806.sh
+++ mt798x/1806.sh
@@ -4,7 +4,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
 #find ./ -name '*mosdns*' -print0 | xargs -0 rm -rf
@@ -22,10 +22,13 @@ rm -rf feeds/luci/applications/luci-app-passwall
 git clone --depth=1 -b master https://github.com/acnixuil/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # argon design
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-#git clone --depth=1 -b main https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
-#git clone --depth=1 -b master https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone --depth=1 -b main https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone --depth=1 -b master https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # mosdns
 git clone --depth=1 -b v5 https://github.com/sbwml/luci-app-mosdns package/mosdns
@@ -33,7 +36,7 @@ git clone --depth=1 -b master https://github.com/sbwml/v2ray-geodata package/v2r
 cp -f package/v2ray-geodata/Makefile feeds/packages/net/v2ray-geodata/Makefile
 
 # ssrp
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone --depth=1 -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 #Enable Cache
 #echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
