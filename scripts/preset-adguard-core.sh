b6a905a1ef64c7843e5b7f4e874482733daa5dd4
acnixuil <acnixuil@gmail.com>
2024-02-03 04:21:51 +0800

    fix

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 mkdir -p files/usr/bin/AdGuardHome
 
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
