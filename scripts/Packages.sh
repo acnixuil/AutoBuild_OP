11ac91a0163bd6bc7a2e094580ac5c7005e0068d
acnixuil <acnixuil@gmail.com>
2024-07-01 12:27:19 +0800

    1

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -34,7 +34,8 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "helloworld" "sbwml/openwrt_helloworld" "v5"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+UPDATE_PACKAGE "openwrt-passwall-packages" "xiaorouji/openwrt-passwall-packages" "main"
 # UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 # UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main" "pkg"
@@ -44,7 +45,6 @@ UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5"
 UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
 UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main"
 UPDATE_PACKAGE "adguardhome" "acnixuil/luci-app-adguardhome" "master"
-UPDATE_PACKAGE "amlogic" "ophub/luci-app-amlogic" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
