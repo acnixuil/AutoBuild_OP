27ae1d5152840f0fb9d59fec15a90b73b87f0c4e
DESKTOP-QOGRJ7I\xin <acnixuil@gmail.com>
2025-08-16 13:10:39 +0800

    update

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -11,7 +11,9 @@ uci set nikki.proxy.ipv6_proxy='0'
 uci set nikki.proxy.proxy_tcp_dport='21 22 80 110 143 194 443 465 853 993 995 8080 8443'
 uci set nikki.proxy.proxy_udp_dport='123 443 8443'
 uci set nikki.mixin.dns_mode='redir-host'
+uci set nikki.mixin.tun_stack='system'
 uci set nikki.mixin.api_secret='qwer1234'
+uci set nikki.authentication.password='qwer1234'
 uci commit nikki
 
 uci set argon.@global[0].mode='dark'
