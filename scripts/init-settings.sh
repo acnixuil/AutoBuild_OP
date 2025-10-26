ab5a6fa15fdc0777e50e28f62ec0714e090ca7f3
DESKTOP-UUT3EQ5\xin <acnixuil@gmail.com>
2025-10-27 07:40:02 +0800

    update

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -7,11 +7,12 @@ uci commit luci
 # nikki
 uci set nikki.config.start_delay='2'
 uci set nikki.proxy.bypass_china_mainland_ip='1'
+uci set nikki.proxy.bypass_china_mainland_ip6='1'
 # uci set nikki.proxy.ipv6_proxy='0'
 uci set nikki.proxy.proxy_tcp_dport='21 22 80 110 143 194 443 465 853 993 995 8080 8443'
 uci set nikki.proxy.proxy_udp_dport='123 443 8443'
-uci set nikki.mixin.dns_mode='redir-host'
-uci set nikki.mixin.tun_stack='system'
+uci delete nikki.mixin.dns_mode='redir-host'
+uci delete nikki.mixin.tun_stack='system'
 uci commit nikki
 
 # momo
