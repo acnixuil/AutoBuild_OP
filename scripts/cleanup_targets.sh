e58daee7802fafe41d78ccbac0b11f4759e4fead
DESKTOP-UUT3EQ5\xin <acnixuil@gmail.com>
2025-11-09 15:02:14 +0800

    update

--- scripts/cleanup_targets.sh
+++ scripts/cleanup_targets.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# file_types=("config.buildinfo" "feeds.buildinfo" "*.manifest" "*-kernel.bin" "*-squashfs-combined.vmdk" "*-rootfs.*" "profiles.json" "sha256sums" "version.buildinfo" "*.itb")
-#
-# dir_path="./"
-#
-# if [ -d "${dir_path}packages" ]; then
-#     rm -rf "${dir_path}packages"
-#     echo "packages directory has been deleted!"
-# fi
-#
-# for file_type in "${file_types[@]}"; do
-#     find "$dir_path" -type f -name "$file_type" -exec rm -rf {} \;
-# done
-#
-# !/bin/bash
-
 dir_path="./"
 keep_patterns=(
     "*-squashfs-factory.bin"
     "*-squashfs-sysupgrade.bin"
+    "*-squashfs-combined.img.gz"
+    "*-squashfs-combined.vmdk"
     "*-squashfs-combined-efi.img.gz"
     "*-squashfs-combined-efi.vmdk"
     "*-squashfs-sysupgrade.itb"
