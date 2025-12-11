c3fbcf288466106175fb71e440bfd15f44362e56
DESKTOP-QOGRJ7I\xin <acnixuil@gmail.com>
2025-12-11 19:27:12 +0800

    update

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -6,8 +6,8 @@ uci commit luci
 
 # nikki
 uci set nikki.config.start_delay='2'
-# uci set nikki.proxy.bypass_china_mainland_ip='1'
-# uci set nikki.proxy.bypass_china_mainland_ip6='1'
+uci set nikki.proxy.bypass_china_mainland_ip='1'
+uci set nikki.proxy.bypass_china_mainland_ip6='1'
 # uci set nikki.proxy.ipv6_proxy='0'
 uci set nikki.proxy.proxy_tcp_dport='21 22 80 110 143 194 443 465 853 993 995 8080 8443'
 uci set nikki.proxy.proxy_udp_dport='123 443 8443'
