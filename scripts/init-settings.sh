c57541ced6b81b30efde9bea55ef38b8ec75b50f
xin <acnixuil@gmail.com>
2026-02-02 16:09:53 +0800

    update

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -6,6 +6,7 @@ uci commit luci
 
 # nikki
 uci set nikki.config.start_delay='2'
+uci set nikki.config.scheduled_restart='1'
 uci set nikki.proxy.bypass_china_mainland_ip='1'
 uci set nikki.proxy.bypass_china_mainland_ip6='1'
 # uci set nikki.proxy.ipv6_proxy='0'
@@ -35,6 +36,9 @@ service dropbear restart
 # uci set dhcp.@dnsmasq[0].min_cache_ttl='600'
 # uci set dhcp.@dnsmasq[0].max_cache_ttl='86400'
 # uci commit dhcp
+
+uci set attendedsysupgrade.client.login_check_for_upgrades='0'
+uci commit attendedsysupgrade
 
 grep -q "/etc/lucky/" /etc/sysupgrade.conf || echo "/etc/lucky/" >> /etc/sysupgrade.conf
 
