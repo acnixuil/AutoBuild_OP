3cb057be32ad2273c7a6965c4c6ad5247beea3e1
xin <acnixuil@gmail.com>
2026-03-31 23:39:04 +0800

    update

--- scripts/Handles.sh
+++ scripts/Handles.sh
@@ -101,7 +101,9 @@ fi
 if [ -d "./momo" ]; then
   section "处理 momo 插件"
   mkdir -p ./momo/luci-app-momo/root/etc/momo/run
-  ln -sf /etc/nikki/run/ui ./momo/luci-app-momo/root/etc/momo/run/ui
+  # ln -sf /etc/nikki/run/ui ./momo/luci-app-momo/root/etc/momo/run/ui
+  UI_TARGET="./momo/luci-app-momo/root/etc/momo/run/ui"
+  download_ui "$UI_TARGET"
   log "momo 面板链接已创建 ✔"
   cd "$PKG_PATCH"
 fi
