39149737d05eab54a24b293bcd03ba2760d14185
xin <acnixuil@gmail.com>
2026-05-14 04:24:16 +0800

    fix package

--- scripts/init-tailscale.sh
+++ scripts/init-tailscale.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -x "/sbin/fw4" ]; then
+    echo ">> 检测到 fw4 (nftables) 环境 (OpenWrt >= 22.03 / 24.10)"
+    FW_MODE="nftables"
+else
+    echo ">> 检测到 fw3 (iptables) 环境 (OpenWrt <= 21.02)"
+    FW_MODE="iptables"
+fi
+
 mkdir -p /etc/tailscale
 touch /etc/config/tailscale
 
@@ -7,24 +15,34 @@ uci -q batch <<EOF
 set tailscale.settings=tailscale
 set tailscale.settings.port='61422'
 set tailscale.settings.state_file='/etc/tailscale/tailscaled.state'
-set tailscale.settings.fw_mode='nftables'
+set tailscale.settings.fw_mode='$FW_MODE'
 set tailscale.settings.log_stderr='1'
 set tailscale.settings.log_stdout='1'
 commit tailscale
 EOF
 
-cat <<EOT > /etc/firewall.user
+if [ "$FW_MODE" = "nftables" ]; then
+    cat <<EOT > /etc/firewall.user
 #!/bin/sh
 nft insert rule inet fw4 output meta nfproto ipv6 udp sport 61422 counter drop
 nft insert rule inet fw4 input meta nfproto ipv6 udp dport 61422 counter drop
 EOT
+else
+    cat <<EOT > /etc/firewall.user
+#!/bin/sh
+ip6tables -I OUTPUT -p udp --sport 61422 -j DROP
+ip6tables -I INPUT -p udp --dport 61422 -j DROP
+EOT
+fi
 chmod +x /etc/firewall.user
 
 uci -q delete firewall.custom_user_script
 uci set firewall.custom_user_script=include
 uci set firewall.custom_user_script.path='/etc/firewall.user'
 uci set firewall.custom_user_script.type='script'
-uci set firewall.custom_user_script.fw4_compatible='1'
+if [ "$FW_MODE" = "nftables" ]; then
+    uci set firewall.custom_user_script.fw4_compatible='1'
+fi
 
 if ! uci show network | grep -q "network.tailscale="; then
     uci set network.tailscale='interface'
